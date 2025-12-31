6579712d24a4caa461a713bd48b1d52c57580106
FedesaGirma <128348288+F43ZA4@users.noreply.github.com>
2025-12-31 11:35:55 +0300

    Update privacy policy link

--- main.py
+++ main.py
@@ -612,7 +612,7 @@ async def start_contact_admin_callback(callback_query: types.CallbackQuery, stat
 
 @dp.message(Command("privacy"), StateFilter(None))
 async def show_privacy(message: types.Message):
-    privacy_policy_url = "https://telegra.ph/Privacy-Policy-for-AAU-Confessions-Bot-04-27"
+    privacy_policy_url = "https://telegra.ph/Privacy-Policy-for-MWU-Confessions-Bot-12-31"
     privacy_text = (
         "<b>Privacy Information</b>\n\n"
         "▪️ Your Telegram User ID is stored but never shown to other users.\n"
@@ -1435,3 +1435,4 @@ if __name__ == "__main__":
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logging.info("Bot stopped.")
+
